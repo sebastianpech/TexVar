62016795a932d3805c46c89b3c025603ae4f4862
unknown <spech@Sebastian-01.office.zt-pech.at>
2015-07-08 15:56:26 +0200

    simple change

--- Beispiel.lua
+++ Beispiel.lua
@@ -1,8 +1,8 @@
-dofile("D:\\Meine Dateien\\Git\\LuaLatexCAS\\tVar.lua")
+require("tVar")
 matrix = require("matrix")
 tVar.numFormat = "%.3f"
 v_1=tVec:New({10,2,7},"v_{1}")
 v_2=tVec:New({3,1,2},"v_{2}")
 v_3 = (v_1+v_2):bracR()-v_2
 v_3:setName("v_{3}")
-print(v_3:printFull())
+print(v_3:printFull)
