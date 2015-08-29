e906b5d6498bbf6c687b1a9b8a46917b30be2772
Specht08 <sebastian.pech@zt-pech.at>
2015-08-29 22:45:16 +0200

    updated doc

--- tVar/constants.lua
+++ tVar/constants.lua
@@ -2,6 +2,8 @@
 -- Global constants
 -- 
 ----------------------------------------------------------------------------
--- tVar.PI PI as tVar with LaTeX representation
+
+--- tVar.PI PI as tVar with LaTeX representation
+-- @return pi as tVar
 tVar.PI = tVar:New(math.pi,"\\pi")
 tVar.PI.eqNum = "\\pi"
