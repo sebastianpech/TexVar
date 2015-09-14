e73e3058cc1ec28e90fadb3442ec81d75336f7e5
Sebastian Pech <sebastian.pech@zt-pech.at>
2015-09-14 08:33:45 +0200

    Added comments to plot.lua

--- README.md
+++ README.md
@@ -8,9 +8,12 @@ See details in doc or CheatSheet
 - LuaTex for use in LaTeX documents. Part of MikTex http://www.luatex.org/
 - Lua Modules
 	- luamatrix
+- GnuPlot 5.0 (change path to gnuplot executable in .config file and run lualatex with --shell-escape)
 - Latex Modules
 	- luacode
 	- amsmath
+	- graphicsx (plotting)
+	- color (debuging)
 
 # INSTALLATION
 Just download the folders and copy lib and tVar into the folder your *.tex script is.
--- tVar/plot.lua
+++ tVar/plot.lua
@@ -4,14 +4,20 @@ tPlot = {
 	terminal = "eps",
 	conf = {},
 }
-
+--- Initialize tPlot table
+--
+-- @param steps (number) Resolution, steps between xmin and xmax
+-- @param stack counts and holds the created plots (globally)
 tPlot.steps = 0.1
 tPlot.stack = {}
 tPlot.stack.add = function(elem)
 	tPlot.stack[#tPlot.stack+1] = elem
 	return #tPlot.stack - 1
 end
-
+--- Create New PLot
+--
+-- @param present (optional) you can pass a previously created plot as template. Set values are overrideable
+-- @retun new Plot
 function tPlot:New(present)
 	local ret = {}
 
@@ -35,41 +41,53 @@ function tPlot:New(present)
 	setmetatable(ret,self)
 	return ret
 end
-
+--- Add function or datapoints {{X1.,Y1}{X2,Y2}} to plot
+--
+-- @param fun functiontion or datapoints
+-- @param title title of the plot
+-- @param style 
 function tPlot:add(fun,title,style)
 	self.fn[#self.fn+1] = {fun,title,style}
 end
-
+--- Pushes a command to the plot command stack
+--
+-- @param command as string
 function tPlot:push(command)
 	self.commands[#self.commands+1]=command
 end
-
+--- Plots the tPot object
+-- 
 function tPlot:plot()
-	--generate plot data X Y
-	
+	-- check if Plot has any functions	
 	if #self.fn == 0 then error("No functions to plot!") end
 
+	-- connect to gnuplot executeable
 	local execState = self.gnuplot_library
 	local gnuplotTerminal = io.popen(execState,"w")
 
+	-- create filename
 	local temp = "tmp_"..tPlot.stack.add(self) .. "." .. self.FileExtension
 
+	-- set terminal and outputpath
 	gnuplotTerminal:write("set terminal "..self.terminal .. " size " .. self.size .. "\n")
 	gnuplotTerminal:write("set output '" .. temp .. "'\n")
 
-	-- check for present commands
+	-- check for present tPlot objects and load the commands
 	if self.conf.presen then
 		for i,v in ipairs(self.present.commands) do
 			gnuplotTerminal:write(v .. "\n")
 		end
 	end
 
+	-- write own commands to gnuplot terminal
 	for i,v in ipairs(self.commands) do
 		gnuplotTerminal:write(v .. "\n")
 	end
 
+	-- start plotting
 	gnuplotTerminal:write("plot")
 
+	-- register functions
 	for i,v in ipairs(self.fn) do
 		if i == #self.fn then
 			gnuplotTerminal:write(" '-' " .. v[3] .. " title '" .. v[2] .. "'")
@@ -78,6 +96,7 @@ function tPlot:plot()
 		end
 	end
 
+	-- write datapoints or functions
 	for i,v in ipairs(self.fn) do
 		if type(v[1]) == "function" then
 			for j=self.conf.xrange.min,self.conf.xrange.max,self.steps do
@@ -91,17 +110,25 @@ function tPlot:plot()
 				gnuplotTerminal:write("\n" .. tVar.Check(w[1]).val .. " " .. tVar.Check(w[2]).val )
 			end
 		end
+		-- end input
 		gnuplotTerminal:write("\ne")
 	end
 
 	gnuplotTerminal:close()
+	-- print latex 
 	tPlot.imgFormat(temp)
 end
-
+--- print path with latex image include
+--
+-- @param path
 function tPlot.imgFormat(path)
   		tex.print("\\includegraphics{" .. path .. "}")
 end
-
+--- translate every acces to undefined object to set method
+--
+-- @param table self
+-- @param key in table
+-- @param value
 function tPlot.forwardCommand(table,key,value)
 	if key == "xrange" then
 		local slt = string.split(value,":")
